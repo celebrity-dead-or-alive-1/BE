8985e4aae9bd14984e0209a0eadddc074dbeefcd
Darrin Lowery <lambda@monsto.com>
2019-12-21 21:17:31 -0600

    backend scaffolded.

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+
+
+server.get('/', (req, res) => {
+	res.send("Running");
+});
+
+module.exports = server;
